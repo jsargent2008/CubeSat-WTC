1fabc0fd93c695b76743b1c0f9e5c45998bd607d
Clayton White <clwht687@gmail.com>
2018-04-23 19:04:01 -0400

    inital PI Commands

---
+++ WTC_Libs/PI_Control/commands.c
@@ -0,0 +1,9 @@
+/*
+ * command.c
+ *
+ *  Created on: Apr 23, 2018
+ *      Author: Clayton White
+ */
+
+#include "commands.h"
+
---
+++ WTC_Libs/PI_Control/commands.h
@@ -0,0 +1,30 @@
+/*
+ * commands.h
+ *
+ * Defines the commands used for bidirectional UART communication for the Raspberry PI.
+ *
+ *  Created on: Apr 23, 2018
+ *      Author: Clayton White
+ */
+
+#ifndef PI_CONTROL_COMMANDS_H_
+#define PI_CONTROL_COMMANDS_H_
+
+
+#define ACK 			"OK"
+#define OK				"OK"
+#define NO				"NO"
+#define SHUTDOWN		"SD"
+#define SHUTDOWN_PRO 	"SP"
+#define REBOOT			"RE"
+#define SEND_FILE 		"SF"
+#define SEND_PACKETS 	"AP"
+#define PING 			"HI"
+#define STATUS			"ST"
+#define CHECK_SIBLING 	"CS"
+#define PIPE_COMMAND 	"PC"
+#define UART_CHECK		"UC"
+
+
+
+#endif /* PI_CONTROL_COMMANDS_H_ */
