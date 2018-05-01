06e025088f2cb945acb2e3fde8ac1674cba8acad
Theodore Cox <tccox3@knights.ucf.edu>
2018-05-01 18:14:52 -0400

    RTC alarm function in progress.

--- WTC_Libs/rtc/rtc.h
+++ WTC_Libs/rtc/rtc.h
@@ -11,7 +11,10 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32l1xx_hal.h"
+#include "stm32l1xx_hal_def.h"
+#include  "stm32l1xx_hal_rtc.h"
 #include "memoryMap\memoryMap.h"
+/* Includes ------------------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
 RTC_TimeTypeDef sTime;
@@ -19,5 +22,6 @@ RTC_DateTypeDef sDate;
 
 /* Private function prototypes -----------------------------------------------*/
 void updateTimeFlash(RTC_HandleTypeDef *hrtc, RTC_TimeTypeDef *sTime, RTC_DateTypeDef *sDate);
+void startRTCAlarm(RTC_HandleTypeDef *hrtc, RTC_AlarmTypeDef *sAlarm);
 
 #endif /* RTC_H_ */
